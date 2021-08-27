b7abe2a14d27693e454ef08191d6f23cf6a0bb25
Bianca Ciotlaus <bianca.ciotlaus@accesa.eu>
2021-08-28 00:13:03 +0300

    End of section 7

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -23,15 +23,12 @@ export class NavComponent implements OnInit {
   {
     this.accountService.login(this.model).subscribe(response => {
       this.router.navigateByUrl('/members');
-    }, error => {
-      console.log(error);
-      this.toastr.error(error.error);
-    });
+    })
   }
 
 logout() {
   this.accountService.logout();
-  this.router.navigateByUrl('/');
+  this.router.navigateByUrl('/')
 }
 
 }
